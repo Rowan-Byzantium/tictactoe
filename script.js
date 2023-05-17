b38b1e35351fa85610f2cfe7fdea728d1a55cd65
Rowan Byzantium <verte735@gmail.com>
2023-05-17 11:58:17 +0200

    switch between symbols

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-//⭕
+//
 //select every elements with list-itm class
 const boxes = document.querySelectorAll(".list-itm");
 // Initialize a counter
@@ -14,8 +14,15 @@ boxes.forEach(function(box){
         //if the box is empty 
         if (this.innerHTML === ""){ 
             counter++;
-            console.log(counter);
-            this.innerHTML = "❌"; //put a cross in the box
+            console.log(counter%2);
+            // if the counter is even then add a circle otherwise add a cross
+            if (counter % 2 === 0) {
+                // add circle in box
+                this.innerHTML = "⭕";
+            }
+            else {
+                this.innerHTML = "❌"; //put a cross in the box
+            }
         }
     
     })
