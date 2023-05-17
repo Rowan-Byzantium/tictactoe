55ed0f6b30a4671b065802d9e35f4b9f4b955315
Rowan Byzantium <verte735@gmail.com>
2023-05-17 16:17:50 +0200

    update status in arrayBoxes on click

--- script.js
+++ script.js
@@ -22,6 +22,7 @@ function displayPlayerTurn() {
  * Create li in HTML (box) with dataset row & col
  * @param {number} row - row number in the grid
  * @param {number} col - column number in the grid
+ * @returns {Element} the new li element
  */
 function createBox(row, col){
     const box = document.createElement("li");
@@ -30,6 +31,7 @@ function createBox(row, col){
     box.classList.add("list-itm");
     box.dataset.row = row;
     box.dataset.col = col;
+    return box;
 }
 
 // -------------- SCRIPT -------------------
@@ -39,23 +41,23 @@ function createBox(row, col){
  * 2 = player2
  */
 const arrayBoxes = [
-    [0, 0, 4], 
-    [0, 7, 0], 
-    [11, 0, 0]
+    [0, 0, 0], 
+    [0, 0, 0], 
+    [0, 0, 0]
 ];
 
+const boxes = [];
+
+//Call createbox function for each column of each row
 for (const row in arrayBoxes){
     for (const col in arrayBoxes[row]){
-        createBox(row, col);
+        boxes.push(createBox(row, col));
     }
 }
 
-//select every elements with list-itm class
-const boxes = document.querySelectorAll(".list-itm");
 // Initialize a counter
 let counter = 0;
 
-console.log(arrayBoxes[2][0]);
 displayPlayerTurn();
 
 //for each boxes 
@@ -66,15 +68,16 @@ boxes.forEach(function (box) {
 
         //if the box is not empty , stop.
         if (this.innerHTML !== "") return;
-        
+    
         // if the counter is even then add a circle otherwise add a cross
         if (counter % 2 === 0) {
             // add circle in box
             this.innerHTML = "⭕";
+            arrayBoxes[this.dataset.row][this.dataset.col] = 1;
         }
         else {
             this.innerHTML = "❌"; //put a cross in the box
-            // console.log("Player2"); 
+            arrayBoxes[this.dataset.row][this.dataset.col] = 2;
         }
         counter++;
         displayPlayerTurn();
